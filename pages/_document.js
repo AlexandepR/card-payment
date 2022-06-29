eec28f29d18c1cd0a8a8b917635c6ddf76872ec0
Alex <alexandepp@gmail.com>
2022-06-29 15:30:52 +0300

    test

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
-import theme from '../pages/payment'; // Adjust here as well
 
 export default class MyDocument extends Document {
     render() {
@@ -19,15 +18,12 @@ export default class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
-
     ctx.renderPage = () =>
         originalRenderPage({
             enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
         });
-
     const initialProps = await Document.getInitialProps(ctx);
 
     return {
