b3c8caa9d46bdacc36f0c871abbf47c143c2c150
Alex <alexandepp@gmail.com>
2022-06-29 16:50:14 +0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,6 @@
 import React from "react";
 import {makeStyles} from "@material-ui/styles";
-export const useStyles = makeStyles({
-    container: {
-        position: 'absolute',
-        left: '50%',
-        transform: 'translate(-50%, 1%)',
-        textAlign: 'center'
-    },
-    field: {
-        boxShadow: '0 3px 5px 2px rgba(0,0,0, .3)',
-        borderRadius: 3,
-    },
-    btn: {
-        border: 'none',
-        margin: 20,
-        width: 400,
-        height: 45,
-        borderRadius: 6,
-        textTransform: 'uppercase',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        cursor: 'pointer',
-        color: '#fff',
-        backgroundSize: '200%',
-        transition: '1s',
-        backgroundPosition: 'right',
-        '&:hover': {
-            backgroundPosition: 'left'
-        }
-    },
-    btn1: {
-        backgroundImage: 'linear-gradient(45deg, #34495e, #9b59b6, #3498db)'
-    }
-})
+
 export default function Home() {
         return (<> </>)
     }
--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {useStyles} from "./index";
+import {makeStyles} from '@material-ui/styles'
 import Head from "next/head";
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {AppBar, Avatar, Box, Button, Container, CssBaseline, Grid, TextField, Typography} from "@mui/material";
@@ -8,12 +8,43 @@ import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import {CurrentRateAPI} from "../api/api";
-import {makeStyles} from '@material-ui/styles'
 // import {Toolbar} from "@material-ui/core";
 // import {InputHandler} from "../component/InputHandler";
 
-export const theme = createTheme({});
 
+export const theme = createTheme({});
+export const useStyles = makeStyles({
+    container: {
+        position: 'absolute',
+        left: '50%',
+        transform: 'translate(-50%, 1%)',
+        textAlign: 'center'
+    },
+    field: {
+        boxShadow: '0 3px 5px 2px rgba(0,0,0, .3)',
+        borderRadius: 3,
+    },
+    btn: {
+        border: 'none',
+        margin: 20,
+        width: 400,
+        height: 45,
+        borderRadius: 6,
+        textTransform: 'uppercase',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        cursor: 'pointer',
+        color: '#fff',
+        backgroundSize: '200%',
+        transition: '1s',
+        backgroundPosition: 'right',
+        '&:hover': {
+            backgroundPosition: 'left'
+        }
+    },
+    btn1: {
+        backgroundImage: 'linear-gradient(45deg, #34495e, #9b59b6, #3498db)'
+    }
+})
 
 export default function Payment() {
     const classes = useStyles()
