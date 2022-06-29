28b6ad4ceb5e2b83bd4bee767af6abb87348fc83
Alex <alexandepp@gmail.com>
2022-06-29 16:55:45 +0300

    test

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -8,8 +8,8 @@ import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import {CurrentRateAPI} from "../api/api";
-// import {Toolbar} from "@material-ui/core";
-// import {InputHandler} from "../component/InputHandler";
+import {Toolbar} from "@material-ui/core";
+import {InputHandler} from "../component/InputHandler";
 
 
 export const theme = createTheme({});
@@ -93,70 +93,70 @@ export default function Payment() {
     return (
         <React.StrictMode>
             <CssBaseline/>
-            {/*<Head>*/}
-            {/*    <title>Payment</title>*/}
-            {/*</Head>*/}
-            {/*<main>*/}
-            {/*    <ThemeProvider theme={theme}>*/}
-            {/*        <Container component="main" maxWidth="xs">*/}
-            {/*            <Box sx={{*/}
-            {/*                marginTop: 6,*/}
-            {/*                display: 'flex',*/}
-            {/*                flexDirection: 'column',*/}
-            {/*                alignItems: 'center',*/}
-            {/*            }}>*/}
-            {/*                <Avatar sx={{height: '50px', width: '50px', m: 2, bgcolor: 'cadetblue'}}>*/}
-            {/*                    <PaymentIcon sx={{height: '35px', width: '35px'}}/>*/}
-            {/*                </Avatar>*/}
-            {/*                <Typography component="h1" variant="h5">*/}
-            {/*                    Payment*/}
-            {/*                </Typography>*/}
-            {/*                <Box component="form"*/}
-            {/*                     noValidate*/}
-            {/*                     onSubmit={handlerSubmit}*/}
-            {/*                     sx={{mt: 3}}>*/}
-            {/*                    <Grid container*/}
-            {/*                          spacing={2}*/}
-            {/*                          justifyContent="center"*/}
-            {/*                          alignItems="center">*/}
-            {/*                        <Grid item xs={12}>*/}
-            {/*                            <InputHandler*/}
-            {/*                                setState={setCard}*/}
-            {/*                                name={"Card number"}*/}
-            {/*                                value={handleCardNumber()}*/}
-            {/*                                maxLength={20}*/}
-            {/*                            />*/}
-            {/*                        </Grid>*/}
-            {/*                        <Grid item xs={12} sm={7}>*/}
-            {/*                            <div className={classes.field}>*/}
-            {/*                                <LocalizationProvider*/}
-            {/*                                    dateAdapter={AdapterDateFns}>*/}
-            {/*                                    <DatePicker*/}
-            {/*                                        inputFormat="MM/yyyy"*/}
-            {/*                                        label="MM/YYYY Date"*/}
-            {/*                                        value={dateCard}*/}
-            {/*                                        onChange={handlerChangeDate}*/}
-            {/*                                        renderInput={(params) => <TextField {...params} />}*/}
-            {/*                                    />*/}
-            {/*                                </LocalizationProvider>*/}
-            {/*                            </div>*/}
-            {/*                        </Grid>*/}
-            {/*                        <Grid item xs={12} sm={5}>*/}
-            {/*                            <InputHandler*/}
-            {/*                                setState={setCvv}*/}
-            {/*                                name={"Cvv"}*/}
-            {/*                                value={cvv}*/}
-            {/*                                maxLength={3}*/}
-            {/*                            />*/}
-            {/*                        </Grid>*/}
-            {/*                        <Grid item xs={12}>*/}
-            {/*                            <InputHandler*/}
-            {/*                                setState={setAmount}*/}
-            {/*                                name={"Amount"}*/}
-            {/*                                value={amount}*/}
-            {/*                            />*/}
-            {/*                        </Grid>*/}
-            {/*                    </Grid>*/}
+            <Head>
+                <title>Payment</title>
+            </Head>
+            <main>
+                <ThemeProvider theme={theme}>
+                    <Container component="main" maxWidth="xs">
+                        <Box sx={{
+                            marginTop: 6,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}>
+                            <Avatar sx={{height: '50px', width: '50px', m: 2, bgcolor: 'cadetblue'}}>
+                                <PaymentIcon sx={{height: '35px', width: '35px'}}/>
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Payment
+                            </Typography>
+                            <Box component="form"
+                                 noValidate
+                                 onSubmit={handlerSubmit}
+                                 sx={{mt: 3}}>
+                                <Grid container
+                                      spacing={2}
+                                      justifyContent="center"
+                                      alignItems="center">
+                                    <Grid item xs={12}>
+                                        <InputHandler
+                                            setState={setCard}
+                                            name={"Card number"}
+                                            value={handleCardNumber()}
+                                            maxLength={20}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={7}>
+                                        <div className={classes.field}>
+                                            <LocalizationProvider
+                                                dateAdapter={AdapterDateFns}>
+                                                <DatePicker
+                                                    inputFormat="MM/yyyy"
+                                                    label="MM/YYYY Date"
+                                                    value={dateCard}
+                                                    onChange={handlerChangeDate}
+                                                    renderInput={(params) => <TextField {...params} />}
+                                                />
+                                            </LocalizationProvider>
+                                        </div>
+                                    </Grid>
+                                    <Grid item xs={12} sm={5}>
+                                        <InputHandler
+                                            setState={setCvv}
+                                            name={"Cvv"}
+                                            value={cvv}
+                                            maxLength={3}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <InputHandler
+                                            setState={setAmount}
+                                            name={"Amount"}
+                                            value={amount}
+                                        />
+                                    </Grid>
+                                </Grid>
                                 <Grid item xs={12} sm={5}>
                                     <div className={classes.container}>
                                         <Button
@@ -173,30 +173,30 @@ export default function Payment() {
                                         </Button>
                                     </div>
                                 </Grid>
-            {/*                </Box>*/}
-            {/*                <Grid>*/}
-            {/*                </Grid>*/}
-            {/*            </Box>*/}
-            {/*        </Container>*/}
-            {/*    </ThemeProvider>*/}
-            {/*</main>*/}
-            {/*<AppBar position="static" elevation={0} component="footer" color="default"*/}
-            {/*        sx={{marginTop: 65}}>*/}
-            {/*</AppBar>*/}
-            {/*<Toolbar style={{*/}
-            {/*    justifyContent: "center"*/}
-            {/*}}>*/}
-            {/*    <Typography variant="caption">©2022 Alexander, alexandepp@gmail.com</Typography>*/}
-            {/*    <a href="https://t.me/Alexandep_R"*/}
-            {/*       // target="_blank"*/}
-            {/*    >*/}
-            {/*        <Avatar sx={{height: '35px', width: '35px', m: 2, bgcolor: '#5C6BC0'}}>*/}
-            {/*            <TelegramIcon*/}
-            {/*                style={{cursor: 'pointer'}}*/}
-            {/*                sx={{height: '25px', width: '25px'}}/>*/}
-            {/*        </Avatar>*/}
-            {/*    </a>*/}
-            {/*</Toolbar>*/}
+                            </Box>
+                            <Grid>
+                            </Grid>
+                        </Box>
+                    </Container>
+                </ThemeProvider>
+            </main>
+            <AppBar position="static" elevation={0} component="footer" color="default"
+                    sx={{marginTop: 65}}>
+            </AppBar>
+            <Toolbar style={{
+                justifyContent: "center"
+            }}>
+                <Typography variant="caption">©2022 Alexander, alexandepp@gmail.com</Typography>
+                <a href="https://t.me/Alexandep_R"
+                   // target="_blank"
+                >
+                    <Avatar sx={{height: '35px', width: '35px', m: 2, bgcolor: '#5C6BC0'}}>
+                        <TelegramIcon
+                            style={{cursor: 'pointer'}}
+                            sx={{height: '25px', width: '25px'}}/>
+                    </Avatar>
+                </a>
+            </Toolbar>
             <CssBaseline/>
         </React.StrictMode>
     )
