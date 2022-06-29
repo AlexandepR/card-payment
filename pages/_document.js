6fa823c98b102721cb87120ad85482307ba0e8aa
Alex <alexandepp@gmail.com>
2022-06-29 16:09:15 +0300

    test

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
 
+
 export default class MyDocument extends Document {
     render() {
         return (
