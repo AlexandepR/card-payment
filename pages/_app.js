a8c8ed353c662f89f4a0fac6361ed56b503ef8f1
Alex <alexandepp@gmail.com>
2022-06-29 18:16:39 +0300

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
-import '../styles/main.css'
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
 
-function MyApp({ Component, pageProps }) {
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
     return (
-    <Component {...pageProps} />
-    )
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
-
-export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/styles';
+import createEmotionServer from '@emotion/server/create-instance';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const originalRenderPage = ctx.renderPage;
+        const cache = createEmotionCache();
+        const { extractCriticalToChunks } = createEmotionServer(cache);
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => function EnhanceApp(props) {
+                    return <App emotionCache={cache} {...props} />;
+                }
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+        const emotionStyles = extractCriticalToChunks(initialProps.html);
+        const emotionStyleTags = emotionStyles.styles.map((style) => (
+            <style
+                data-emotion={`${style.key} ${style.ids.join(' ')}`}
+                key={style.key}
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{ __html: style.css }}
+            />
+        ));
+
+        return {
+            ...initialProps,
+            emotionStyleTags,
+        };
+    }
+
     render() {
         return (
             <Html lang="en">
-
                 <Head>
+                    <meta name="theme-color" content={theme.palette.primary.main} />
+                    <link rel="shortcut icon" href="/static/favicon.ico" />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    {this.props.emotionStyleTags}
                 </Head>
                 <body>
                 <Main />
@@ -16,19 +53,4 @@ export default class MyDocument extends Document {
             </Html>
         );
     }
-}
-
-MyDocument.getInitialProps = async (ctx) => {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () =>
-        originalRenderPage({
-            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-        });
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-        ...initialProps,
-        styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-    };
 };
